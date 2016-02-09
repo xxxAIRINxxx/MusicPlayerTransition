cb7ad05aa9e2bbd93857647ab71dddff7e13d046
Mehdi Shibahara <CO2Omehdi@mehdis-macbook-pro.local>
2016-02-09 11:18:50 +0900

    Removed alpha effects

--- MusicPlayerTransition/MusicPlayerTransition/ViewController.swift
+++ MusicPlayerTransition/MusicPlayerTransition/ViewController.swift
@@ -77,10 +77,6 @@ class ViewController: UIViewController {
                 self!.miniPlayerView.frame.origin.y = startOriginY
                 self!.modalVC.view.frame.origin.y = self!.miniPlayerView.frame.origin.y + self!.miniPlayerView.frame.size.height
                 self!.tabBar.frame.origin.y = tabStartOriginY
-                self!.containerView.alpha = 1.0
-                for subview in self!.miniPlayerView.subviews {
-                    subview.alpha = 1.0
-                }
             }
             
             self!.animator.presentationAnimationHandler = { [weak self] (containerView: UIView, percentComplete: CGFloat) in
@@ -95,10 +91,6 @@ class ViewController: UIViewController {
                     self!.tabBar.frame.origin.y = tabEndOriginY
                 }
                 
-                self!.containerView.alpha = 1.0 - (1.0 * _percentComplete) + 0.5
-                for subview in self!.miniPlayerView.subviews {
-                    subview.alpha = 1.0 - (1.0 * percentComplete)
-                }
             }
             
             self!.animator.presentationCompletionHandler = {(containerView: UIView, completeTransition: Bool) in
@@ -136,10 +128,6 @@ class ViewController: UIViewController {
                 self!.miniPlayerView.frame.origin.y = startOriginY
                 self!.modalVC.view.frame.origin.y = self!.miniPlayerView.frame.origin.y + self!.miniPlayerView.frame.size.height
                 self!.tabBar.frame.origin.y = tabStartOriginY
-                self!.containerView.alpha = 0.0
-                for subview in self!.miniPlayerView.subviews {
-                    subview.alpha = 0.0
-                }
             }
             
             self!.animator.dismissalAnimationHandler = {(containerView: UIView, percentComplete: CGFloat) in
@@ -147,11 +135,7 @@ class ViewController: UIViewController {
                 self!.miniPlayerView.frame.origin.y = startOriginY + (diff * _percentComplete)
                 self!.modalVC.view.frame.origin.y = self!.miniPlayerView.frame.origin.y + self!.miniPlayerView.frame.size.height
                 self!.tabBar.frame.origin.y = tabStartOriginY - (tabDiff *  _percentComplete)
-                
-                self!.containerView.alpha = (1.0 * _percentComplete)
-                for subview in self!.miniPlayerView.subviews {
-                    subview.alpha = 1.0 * _percentComplete
-                }
+
             }
             
             self!.animator.dismissalCompletionHandler = { (containerView: UIView, completeTransition: Bool) in
