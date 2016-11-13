e62428ebf3a64169f71cc537fd84ca0b37b7d358
xxxAIRINxxx <xl1138@gmail.com>
2016-11-13 13:27:39 +0900

    Add MusicPlayerTransitionAnimation

---
+++ MusicPlayerTransition/MusicPlayerTransition/MusicPlayerTransitionAnimation.swift
@@ -0,0 +1,158 @@
+//
+//  MusicPlayerTransitionAnimation.swift
+//  MusicPlayerTransition
+//
+//  Created by xxxAIRINxxx on 2016/11/05.
+//  Copyright © 2016 xxxAIRINxxx. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import ARNTransitionAnimator
+
+final class MusicPlayerTransitionAnimation : TransitionAnimatable {
+    
+    fileprivate weak var rootVC: ViewController!
+    fileprivate weak var modalVC: ModalViewController!
+    
+    var completion: ((Bool) -> Void)?
+    
+    private let miniPlayerStartFrame: CGRect
+    private let tabBarStartFrame: CGRect
+    
+    private var containerView: UIView?
+    
+    deinit {
+        print("deinit MusicPlayerTransitionAnimation")
+    }
+    
+    init(rootVC: ViewController, modalVC: ModalViewController) {
+        self.rootVC = rootVC
+        self.modalVC = modalVC
+        
+        self.miniPlayerStartFrame = rootVC.miniPlayerView.frame
+        self.tabBarStartFrame = rootVC.tabBar.frame
+    }
+    
+    // @see : http://stackoverflow.com/questions/25588617/ios-8-screen-blank-after-dismissing-view-controller-with-custom-presentation
+    func prepareContainer(_ transitionType: TransitionType, containerView: UIView, from fromVC: UIViewController, to toVC: UIViewController) {
+        self.containerView = containerView
+        if transitionType.isPresenting {
+            self.modalVC.view.removeFromSuperview()
+            self.rootVC.view.insertSubview(self.modalVC.view, belowSubview: self.rootVC.tabBar)
+        } else {
+//            self.modalVC.view.removeFromSuperview()
+            self.rootVC.view.insertSubview(self.modalVC.view, belowSubview: self.rootVC.tabBar)
+        }
+    }
+    
+    func willAnimation(_ transitionType: TransitionType, containerView: UIView) {
+        if transitionType.isPresenting {
+            self.rootVC.beginAppearanceTransition(true, animated: false)
+            
+            self.rootVC.view.setNeedsLayout()
+            self.rootVC.view.layoutIfNeeded()
+            self.modalVC.view.setNeedsLayout()
+            self.modalVC.view.layoutIfNeeded()
+            
+            self.modalVC.view.frame.origin.y = self.rootVC.miniPlayerView.frame.origin.y + self.rootVC.miniPlayerView.frame.size.height
+        } else {
+            self.rootVC.beginAppearanceTransition(false, animated: false)
+            
+            self.rootVC.view.setNeedsLayout()
+            self.rootVC.view.layoutIfNeeded()
+            self.modalVC.view.setNeedsLayout()
+            self.modalVC.view.layoutIfNeeded()
+            
+            self.rootVC.miniPlayerView.alpha = 1.0
+            self.rootVC.miniPlayerView.frame.origin.y = -self.rootVC.miniPlayerView.bounds.size.height
+            self.rootVC.tabBar.frame.origin.y = containerView.bounds.size.height
+        }
+    }
+    
+    func updateAnimation(_ transitionType: TransitionType, percentComplete: CGFloat) {
+        print(percentComplete)
+        if transitionType.isPresenting {
+            // miniPlayerView
+            let startOriginY = self.miniPlayerStartFrame.origin.y
+            let endOriginY = -self.miniPlayerStartFrame.size.height
+            let diff = -endOriginY + startOriginY
+            // tabBar
+            let tabStartOriginY = self.tabBarStartFrame.origin.y
+            let tabEndOriginY = self.modalVC.view.frame.size.height
+            let tabDiff = tabEndOriginY - tabStartOriginY
+            
+            let playerY = startOriginY - (diff * percentComplete)
+            self.rootVC.miniPlayerView.frame.origin.y = max(min(playerY, self.miniPlayerStartFrame.origin.y), endOriginY)
+
+            self.modalVC.view.frame.origin.y = self.rootVC.miniPlayerView.frame.origin.y + self.rootVC.miniPlayerView.frame.size.height
+            let tabY = tabStartOriginY + (tabDiff * percentComplete)
+            self.rootVC.tabBar.frame.origin.y = min(max(tabY, self.tabBarStartFrame.origin.y), tabEndOriginY)
+            
+            let alpha = 1.0 - (1.0 * percentComplete)
+            self.rootVC.containerView.alpha = alpha + 0.5
+            self.rootVC.tabBar.alpha = alpha
+            self.rootVC.miniPlayerView.subviews.forEach { $0.alpha = alpha }
+        } else {
+            // miniPlayerView
+            let startOriginY = 0 - self.rootVC.miniPlayerView.bounds.size.height
+            let endOriginY = self.miniPlayerStartFrame.origin.y
+            let diff = -startOriginY + endOriginY
+            // tabBar
+            let tabStartOriginY = self.rootVC.containerView.bounds.size.height
+            let tabEndOriginY = self.tabBarStartFrame.origin.y
+            let tabDiff = tabStartOriginY - tabEndOriginY
+            
+            self.rootVC.miniPlayerView.frame.origin.y = startOriginY + (diff * percentComplete)
+            self.modalVC.view.frame.origin.y = self.rootVC.miniPlayerView.frame.origin.y + self.rootVC.miniPlayerView.frame.size.height
+            
+            self.rootVC.tabBar.frame.origin.y = tabStartOriginY - (tabDiff *  percentComplete)
+            
+            let alpha = 1.0 * percentComplete
+            self.rootVC.containerView.alpha = alpha + 0.5
+            self.rootVC.tabBar.alpha = alpha
+            self.rootVC.miniPlayerView.alpha = 1.0
+            self.rootVC.miniPlayerView.subviews.forEach { $0.alpha = alpha }
+        }
+    }
+    
+    func finishAnimation(_ transitionType: TransitionType, didComplete: Bool) {
+        self.rootVC.endAppearanceTransition()
+        
+        if transitionType.isPresenting {
+            if didComplete {
+                self.rootVC.miniPlayerView.alpha = 0.0
+                self.modalVC.view.removeFromSuperview()
+                self.containerView?.addSubview(self.modalVC.view)
+                
+                self.completion?(transitionType.isPresenting)
+            } else {
+                self.rootVC.beginAppearanceTransition(true, animated: false)
+                self.rootVC.endAppearanceTransition()
+            }
+        } else {
+            if didComplete {
+                self.modalVC.view.removeFromSuperview()
+                
+                self.completion?(transitionType.isPresenting)
+            } else {
+                self.rootVC.miniPlayerView.alpha = 0.0
+                
+                self.modalVC.view.removeFromSuperview()
+                self.containerView?.addSubview(self.modalVC.view)
+                
+                self.rootVC.beginAppearanceTransition(false, animated: false)
+                self.rootVC.endAppearanceTransition()
+            }
+        }
+    }
+}
+
+extension MusicPlayerTransitionAnimation {
+    
+    func sourceVC() -> UIViewController { return self.rootVC }
+    
+    func destVC() -> UIViewController { return self.modalVC }
+}
+
+
--- MusicPlayerTransition/MusicPlayerTransition/ViewController.swift
+++ MusicPlayerTransition/MusicPlayerTransition/ViewController.swift
@@ -11,12 +11,12 @@ import ARNTransitionAnimator
 
 final class ViewController: UIViewController {
     
-    @IBOutlet weak var containerView : UIView!
-    @IBOutlet weak var tabBar : UITabBar!
-    @IBOutlet weak var miniPlayerView : LineView!
-    @IBOutlet weak var miniPlayerButton : UIButton!
+    @IBOutlet fileprivate(set) weak var containerView : UIView!
+    @IBOutlet fileprivate(set) weak var tabBar : UITabBar!
+    @IBOutlet fileprivate(set) weak var miniPlayerView : LineView!
+    @IBOutlet fileprivate(set) weak var miniPlayerButton : UIButton!
     
-    fileprivate var animator : ARNTransitionAnimator!
+    private var animator : ARNTransitionAnimator?
     fileprivate var modalVC : ModalViewController!
 
     override func viewDidLoad() {
@@ -25,9 +25,6 @@ final class ViewController: UIViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         self.modalVC = storyboard.instantiateViewController(withIdentifier: "ModalViewController") as? ModalViewController
         self.modalVC.modalPresentationStyle = .overFullScreen
-        self.modalVC.tapCloseButtonActionHandler = { [unowned self] in
-            self.animator.interactiveType = .None
-        }
         
         let color = UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 0.3)
         self.miniPlayerButton.setBackgroundImage(self.generateImageWithColor(color), for: .highlighted)
@@ -37,163 +34,39 @@ final class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         print("ViewController viewWillAppear")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        
         print("ViewController viewWillDisappear")
     }
     
     func setupAnimator() {
-        self.animator = ARNTransitionAnimator(operationType: .Present, fromVC: self, toVC: self.modalVC)
-        self.animator.usingSpringWithDamping = 0.8
-        self.animator.gestureTargetView = self.miniPlayerView
-        self.animator.interactiveType = .Present
-        
-        // Present
-        
-        self.animator.presentationBeforeHandler = { [unowned self] containerView, transitionContext in
-            print("start presentation")
-            self.beginAppearanceTransition(false, animated: false)
-            
-            self.animator.direction = .Top
-            
-            self.modalVC.view.frame.origin.y = self.miniPlayerView.frame.origin.y + self.miniPlayerView.frame.size.height
-            self.view.insertSubview(self.modalVC.view, belowSubview: self.tabBar)
-            
-            self.view.layoutIfNeeded()
-            self.modalVC.view.layoutIfNeeded()
-            
-            // miniPlayerView
-            let startOriginY = self.miniPlayerView.frame.origin.y
-            let endOriginY = -self.miniPlayerView.frame.size.height
-            let diff = -endOriginY + startOriginY
-            // tabBar
-            let tabStartOriginY = self.tabBar.frame.origin.y
-            let tabEndOriginY = containerView.frame.size.height
-            let tabDiff = tabEndOriginY - tabStartOriginY
-            
-            self.animator.presentationCancelAnimationHandler = { containerView in
-                self.miniPlayerView.frame.origin.y = startOriginY
-                self.modalVC.view.frame.origin.y = self.miniPlayerView.frame.origin.y + self.miniPlayerView.frame.size.height
-                self.tabBar.frame.origin.y = tabStartOriginY
-                self.containerView.alpha = 1.0
-                self.tabBar.alpha = 1.0
-                self.miniPlayerView.alpha = 1.0
-                for subview in self.miniPlayerView.subviews {
-                    subview.alpha = 1.0
-                }
-            }
-            
-            self.animator.presentationAnimationHandler = { [unowned self] containerView, percentComplete in
-                let _percentComplete = percentComplete >= 0 ? percentComplete : 0
-                self.miniPlayerView.frame.origin.y = startOriginY - (diff * _percentComplete)
-                if self.miniPlayerView.frame.origin.y < endOriginY {
-                    self.miniPlayerView.frame.origin.y = endOriginY
-                }
-                self.modalVC.view.frame.origin.y = self.miniPlayerView.frame.origin.y + self.miniPlayerView.frame.size.height
-                self.tabBar.frame.origin.y = tabStartOriginY + (tabDiff * _percentComplete)
-                if self.tabBar.frame.origin.y > tabEndOriginY {
-                    self.tabBar.frame.origin.y = tabEndOriginY
-                }
-                
-                let alpha = 1.0 - (1.0 * _percentComplete)
-                self.containerView.alpha = alpha + 0.5
-                self.tabBar.alpha = alpha
-                for subview in self.miniPlayerView.subviews {
-                    subview.alpha = alpha
-                }
-            }
-            
-            self.animator.presentationCompletionHandler = { containerView, completeTransition in
-                self.endAppearanceTransition()
-                
-                if completeTransition {
-                    self.miniPlayerView.alpha = 0.0
-                    self.modalVC.view.removeFromSuperview()
-                    containerView.addSubview(self.modalVC.view)
-                    self.animator.interactiveType = .Dismiss
-                    self.animator.gestureTargetView = self.modalVC.view
-                    self.animator.direction = .Bottom
-                } else {
-                    self.beginAppearanceTransition(true, animated: false)
-                    self.endAppearanceTransition()
-                }
+        let animation = MusicPlayerTransitionAnimation(rootVC: self, modalVC: self.modalVC)
+        animation.completion = { [weak self] isPresenting in
+            if isPresenting {
+                guard let _self = self else { return }
+                let modalGestureHandler = TransitionGestureHandler(targetVC: _self, direction: .bottom)
+                modalGestureHandler.registerGesture(_self.modalVC.view)
+                _self.animator?.registerInteractiveTransitioning(.dismiss, gestureHandler: modalGestureHandler)
+            } else {
+                self?.setupAnimator()
             }
         }
         
-        // Dismiss
-        
-        self.animator.dismissalBeforeHandler = { [unowned self] containerView, transitionContext in
-            print("start dismissal")
-            self.beginAppearanceTransition(true, animated: false)
-            
-            self.view.insertSubview(self.modalVC.view, belowSubview: self.tabBar)
-            
-            self.view.layoutIfNeeded()
-            self.modalVC.view.layoutIfNeeded()
-            
-            // miniPlayerView
-            let startOriginY = 0 - self.miniPlayerView.bounds.size.height
-            let endOriginY = self.containerView.bounds.size.height - self.tabBar.bounds.size.height - self.miniPlayerView.frame.size.height
-            let diff = -startOriginY + endOriginY
-            // tabBar
-            let tabStartOriginY = containerView.bounds.size.height
-            let tabEndOriginY = containerView.bounds.size.height - self.tabBar.bounds.size.height
-            let tabDiff = tabStartOriginY - tabEndOriginY
-            
-            self.tabBar.frame.origin.y = containerView.bounds.size.height
-            self.containerView.alpha = 0.5
-            
-            self.animator.dismissalCancelAnimationHandler = { containerView in
-                self.miniPlayerView.frame.origin.y = startOriginY
-                self.modalVC.view.frame.origin.y = self.miniPlayerView.frame.origin.y + self.miniPlayerView.frame.size.height
-                self.tabBar.frame.origin.y = tabStartOriginY
-                self.containerView.alpha = 0.5
-                self.tabBar.alpha = 0.0
-                self.miniPlayerView.alpha = 0.0
-                for subview in self.miniPlayerView.subviews {
-                    subview.alpha = 0.0
-                }
-            }
-            
-            self.animator.dismissalAnimationHandler = { containerView, percentComplete in
-                let _percentComplete = percentComplete >= -0.05 ? percentComplete : -0.05
-                self.miniPlayerView.frame.origin.y = startOriginY + (diff * _percentComplete)
-                self.modalVC.view.frame.origin.y = self.miniPlayerView.frame.origin.y + self.miniPlayerView.frame.size.height
-                self.tabBar.frame.origin.y = tabStartOriginY - (tabDiff *  _percentComplete)
-                
-                let alpha = 1.0 * _percentComplete
-                self.containerView.alpha = alpha + 0.5
-                self.tabBar.alpha = alpha
-                self.miniPlayerView.alpha = 1.0
-                for subview in self.miniPlayerView.subviews {
-                    subview.alpha = alpha
-                }
-            }
-            
-            self.animator.dismissalCompletionHandler = { containerView, completeTransition in
-                self.endAppearanceTransition()
-                
-                if completeTransition {
-                    self.modalVC.view.removeFromSuperview()
-                    self.animator.gestureTargetView = self.miniPlayerView
-                    self.animator.interactiveType = .Present
-                } else {
-                    self.modalVC.view.removeFromSuperview()
-                    containerView.addSubview(self.modalVC.view)
-                    self.beginAppearanceTransition(false, animated: false)
-                    self.endAppearanceTransition()
-                }
-            }
-        }
+        let gestureHandler = TransitionGestureHandler(targetVC: self, direction: .top)
+        gestureHandler.registerGesture(self.miniPlayerView)
+
+        self.animator = ARNTransitionAnimator(duration: 0.5, animation: animation)
+        self.animator?.registerInteractiveTransitioning(.present, gestureHandler: gestureHandler)
         
         self.modalVC.transitioningDelegate = self.animator
     }
     
     @IBAction func tapMiniPlayerButton() {
-        self.animator.interactiveType = .None
         self.present(self.modalVC, animated: true, completion: nil)
     }
     
